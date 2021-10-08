bb95e7d7fe33d297966aca45cf03a42060b9e007
x91003502 <x91003502@gmail.com>
2021-10-09 00:52:44 +0800

    Utilized PurrWeb.Endpoint.broadcast to update messages. Utilized phx-update="append" tag in div for efficient message update.

--- lib/purr_web/live/room_live.ex
+++ lib/purr_web/live/room_live.ex
@@ -7,12 +7,26 @@ defmodule PurrWeb.RoomLive do
     Logger.info(room_id)
     topic = "room:" <> room_id
     PurrWeb.Endpoint.subscribe(topic)
-    {:ok, assign(socket, room_id: room_id, topic: topic, messages: ["Hi, there!"])}
+    {:ok,
+      assign(
+        socket,
+        room_id: room_id,
+        topic: topic,
+        messages: [%{uuid: UUID.uuid4(), content: "Hi, there!"}],
+        temporary_assigns: [messages: []]
+    )}
   end
 
   @impl true
   def handle_event("submit_message", %{"message_form" => %{"message" => message}}, socket) do
-    Logger.info(message)
+    message_payload = %{uuid: UUID.uuid4(), content: message}
+    PurrWeb.Endpoint.broadcast(socket.assigns.topic, "update-message", message_payload)
     {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%{event: "update-message", payload: message_payload}, socket) do
+    Logger.info(message_payload)
+    {:noreply, assign(socket, messages: [message_payload])}
   end
 end
